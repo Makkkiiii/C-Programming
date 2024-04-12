d848b6e6234bf8e4321bdb20f735ef7bcea2aa82
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-04-12 09:25:52 +0545

    Added Simple C program to use "Comment"

---
+++ comment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	//This is a simple program to understand comment or "//" 
+	// This type of comment is known as Single Line Comment
+	
+	/* This is used to do Multi Line Comment
+	
+	
+	
+	
+	
+	
+	 */
+	
+	return 0;
+}
