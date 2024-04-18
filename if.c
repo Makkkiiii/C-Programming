0f460bbde2e7b946bd1530606f401e31352a968d
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-04-18 13:13:42 +0545

    Simple IF program

---
+++ if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Enter value of a: ");
+	scanf("%d", &a);
+	if (a>=0)
+	{
+		printf("The number is Positive");
+	}
+	else
+	{
+		printf("The number is negative");
+	}
+	return 0;
+}
